b4f7337db7ae078ed0d0c2dfa3ea827093bffbfc
Helen Jones <helenfrancesjones@gmail.com>
2023-03-08 01:01:10 -0600

    added to form submission function

--- scriptHelper.js
+++ scriptHelper.js
@@ -27,37 +27,55 @@ function validateInput(testInput) {
       }
    }
 
-//copilot and pilot have to be strings, fuel and cargo levels have to be numbers
 
 
-//update pilot/copilot status to include names
-pilotStatus.innerHTML = `Pilot ${pilot} is ready.`;
-copilotStatus.innerHTML = `Co-pilot ${copilot} is ready.`;
-list.style.visibility = 'hidden';
 
-//update faulty items: change color (red=not ready, green=ready) based on fuel/cargo levels
-   if(Number(fuelLevel) < 10000){
-    fuelStatus.innerHTML = `Not enough fuel for journey.`;
-    launchStatus.innerHTML = `Shuttle not ready for launch.`;
-    launchStatus.style.color = `red`;
-    list.style.visibility = 'visible';
-   } else if (Number(cargoLevel) > 10000){
-    cargoStatus.innerHTML = `Cargo too heavy for takeoff.`;
-    launchStatus.innerHTML = `Shuttle not ready for launch.`;
-    launchStatus.style.color = `red`;
-    list.style.visibility = 'visible';
-   } else if (Number(cargoLevel) < 10000 && Number(fuelLevel > 10000)){
-    cargoStatus.innerHTML = `Cargo light enough for takeoff.`;
-    fuelStatus.innerHTML = `Enough fuel for journey.`;
-    launchStatus.innerHTML = `Shuttle is ready for launch.`;
-    launchStatus.style.color = `green`;
-    list.style.visibilty = 'visible';
-   }
+
 
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   //DOM element list
+        let pilotStatus = document.getElementById('pilotStatus');
+        let copilotStatus = document.getElementById('copilotStatus');
+        let fuelStatus = document.getElementById('fuelStatus');
+        let launchStatus = document.getElementById('launchStatus');
+        let cargoStatus = document.getElementById('cargoStatus');
+    //fields cant be left empty: use *alerts* to validate this
+        if (validateInput(pilot) === "" || validateInput(copilot) === "" ||
+        validateInput(fuelLevel) === "" || validateInput(cargoLevel) === "") {
+            alert(`All fields required.`);
+        }
+    //copilot and pilot have to be strings, fuel and cargo levels have to be numbers{
+        else if (validateInput(fuelLevel) === NaN || validateInput(cargoLevel) === NaN) {
+                alert(`Make sure to enter valid information for each field!`)
+            } else if (validateInput(pilot) === Number || validateInput(copilot) === Number){
+                alert(`Make sure to enter valid information for each field!`)
+            } else {
+            //update pilot/copilot status to include names
+                pilotStatus.innerHTML = `Pilot ${pilot} is ready.`;
+                copilotStatus.innerHTML = `Co-pilot ${copilot} is ready.`;
+                list.style.visibility = 'hidden';
+            }
+        //update faulty items: change color (red=not ready, green=ready) based on fuel/cargo levels
+        if(Number(fuelLevel) < 10000){
+        fuelStatus.innerHTML = `Not enough fuel for journey.`;
+        launchStatus.innerHTML = `Shuttle not ready for launch.`;
+        launchStatus.style.color = `red`;
+        list.style.visibility = 'visible';
+         } else if (Number(cargoLevel) > 10000){
+            cargoStatus.innerHTML = `Cargo too heavy for takeoff.`;
+            launchStatus.innerHTML = `Shuttle not ready for launch.`;
+            launchStatus.style.color = `red`;
+            list.style.visibility = 'visible';
+            } else if (Number(cargoLevel) < 10000 && Number(fuelLevel > 10000)){
+                cargoStatus.innerHTML = `Cargo light enough for takeoff.`;
+                fuelStatus.innerHTML = `Enough fuel for journey.`;
+                launchStatus.innerHTML = `Shuttle is ready for launch.`;
+                launchStatus.style.color = `green`;
+                list.style.visibilty = 'visible';
+   }
 }
+    
 
 async function myFetch() {
     let planetsReturned;
